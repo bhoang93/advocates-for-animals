c63f72d45e5c65c54cff7254a4e28717d0b315f1
Brian Hoang <bhoang35@gmail.com>
2019-04-28 10:34:33 +0100

    changed news to blog

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,7 +6,7 @@ const pages = [
   { label: "About Us", link: "about" },
   { label: "Areas of Expertise", link: "expertise" },
   { label: "What Clients Say", link: "clients" },
-  { label: "News", link: "news" },
+  { label: "Blog", link: "blog" },
   { label: "Contact Us", link: "contact" }
 ];
 
