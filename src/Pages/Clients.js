e72cebb6d870effc3c40938532adab8d92e92b70
Brian Hoang <bhoang35@gmail.com>
2019-06-10 07:33:52 +0100

    added clients

--- src/Pages/Clients.js
+++ src/Pages/Clients.js
@@ -38,6 +38,16 @@ const clientQuotes = [
     quote:
       "David has provided excellent quality legal advice to Animal Aid. It is so reassuring to work with someone who has such brilliant legal expertise, but also understands the specific issues relating to animal protection, and shares our dedication to making the world a better place for animals. As well as being extremely helpful, David’s advice has always been presented to us in a clear, non-technical way, which makes the process of dealing with legal issues so much easier. I would have full confidence in recommending David to any animal protection organisation who was looking for legal advice.",
     attr: "- Animal Aid"
+  },
+  {
+    quote:
+      "The services set out by Advocates for Animals are absolutely invaluable to animal protection NGOs aiming to take the most direct route to creating sustained change for animals, to maximise impact, and to minimise risk. At Humane Society International I fully anticipate we would instruct the firm, particularly with regard to the complex legal landscape around trade as the U.K. departs the EU, an area in which David is extremely well versed. Having sought and used advice from the solicitors behind this firm before I can attest not only to their in-depth knowledge of the law as it relates to animal protection, but also to their understanding and accommodation of the varying strategies, positions and messaging held by their clients. This new law firm is a logical and much-needed step to increase the effectiveness and efficiency of the animal protection movement in the U.K.",
+    attr: "- Humane Society International"
+  },
+  {
+    quote:
+      "Advocates for Animals provide a unique blend of legal expertise and passion for campaigning to make the world a better place for animals. This means they don’t discourage clients from taking risks if the benefits to animals could be great, but rather advise on ways to mitigate risks or even suggest better ideas based on their long history of work in this sector. They are always professional, responsive and committed to achieving the best possible outcome for animals while also protecting the charities and activists they represent. I can’t recommend them highly enough.",
+    attr: "- Animal Equality"
   }
 ];
 
--- src/Pages/Donate.js
+++ src/Pages/Donate.js
@@ -6,11 +6,13 @@ const Donate = () => {
     <div className="donate">
       <h2 className="sub-heading">Donate</h2>
       <p className="donate__content">
-        In addition to helping animal protection organisations through use of
-        the law, Advocates for Animals would like the freedom to bring cases on
-        behalf of itself that we feel are essential to help animals. The
-        practice of law can be expensive. If you would like to ensure that AfA
-        can bring these cases, please donate. Thank you.
+        The practice of law can be expensive, if you would like to ensure that
+        Advocates for Animals can help out as many groups as possible at an
+        affordable rate we would appreciate your support. In addition to helping
+        animal protection organisations through use of the law, Advocates for
+        Animals would like the freedom to bring cases on behalf of itself that
+        we feel are essential to help animals. If you would like to ensure that
+        we can bring these cases, please donate. Thank you.
       </p>
       <form
         action="https://www.paypal.com/cgi-bin/webscr"
